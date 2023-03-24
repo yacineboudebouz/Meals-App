fecdd980a12971868948c0105896f04dce446270
yacineboudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2023-03-24 16:11:19 +0000

    style

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,17 +1,29 @@
+import 'package:delimeals/models/category.dart';
 import 'package:flutter/material.dart';
+import 'dummy.dart';
+import 'package:delimeals/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: [],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals App'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+        // ignore: sort_child_properties_last
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:delimeals/categories_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,31 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'DeliMeals  ',
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromARGB(255, 254, 229, 1),
+      ),
+      title: 'DeliMeals',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DeliMeals"),
-      ),
-      body: const Center(
-        child: Text("Navigation Time"),
-      ),
+      home: const CategoriesScreen(),
     );
   }
 }
