e2f35cfe0938ae02482145482a71c5a593b5a662
yacineboudebouz <104728737+yacineboudebouz@users.noreply.github.com>
2023-03-24 14:54:50 +0000

    add categories list

---
+++ lib/dummy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:delimeals/categories_screen.dart';
+import 'models/category.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_CATEGORIES = [
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.green),
+  Category(id: 'c3', title: 'Hamburgers', color: Colors.brown),
+  Category(id: 'c4', title: 'Algerian', color: Colors.blue),
+  Category(id: 'c5', title: 'Tacos', color: Colors.orange),
+  Category(id: 'c6', title: 'American', color: Colors.lime),
+  Category(id: 'c7', title: 'Fast food', color: Colors.cyanAccent),
+  Category(id: 'c8', title: 'Japanian', color: Colors.amber),
+  Category(id: 'c9', title: 'Summer', color: Colors.indigo),
+  Category(id: 'c10', title: 'Winter', color: Colors.pink)
+];
